434f6b1c26846a78b2b65cda22fa6a1b5313b1bd
xianyumht-cmd <xianyumht@gmail.com>
2026-08-06 18:38:26 +0800

    Version the floating-ball release as FACM 2.1

--- src/FACM/Properties/AssemblyInfo.cs
+++ src/FACM/Properties/AssemblyInfo.cs
@@ -2,12 +2,12 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("FACM")]
-[assembly: AssemblyDescription("透明、可审计的 Windows 游戏残留清理工具")]
+[assembly: AssemblyDescription("轻量桌面悬浮球与客户端实用工具")]
 [assembly: AssemblyCompany("FACM")]
 [assembly: AssemblyProduct("FACM")]
 [assembly: AssemblyCopyright("Copyright © 2026 FACM")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9e0afd05-17e7-4c6a-a8d3-09d45c8d8ec7")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
