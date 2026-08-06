4b49db10f689b043ef789f2514c514ccda76c15b
xianyumht-cmd <xianyumht@gmail.com>
2026-08-07 00:36:08 +0800

    升级 FACM 版本到 3.1.0

--- src/FACM/Properties/AssemblyInfo.cs
+++ src/FACM/Properties/AssemblyInfo.cs
@@ -2,12 +2,12 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("FACM")]
-[assembly: AssemblyDescription("FACM desktop control center with configurable cleanup and verified embedded tools")]
+[assembly: AssemblyDescription("FACM desktop control center with configurable cleanup, verified tool bundle, online updates and announcements")]
 [assembly: AssemblyCompany("FACM")]
 [assembly: AssemblyProduct("FACM")]
 [assembly: AssemblyCopyright("Copyright © 2026 FACM")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9e0afd05-17e7-4c6a-a8d3-09d45c8d8ec7")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyInformationalVersion("3.1.0")]
