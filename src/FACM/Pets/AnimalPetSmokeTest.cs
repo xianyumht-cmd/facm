ffe783e8f1011dea464bcce26da787702b474c4b
xianyumht-cmd <xianyumht@gmail.com>
2026-08-07 20:48:12 +0800

    test: validate professional animal artwork and smooth renderer

--- src/FACM/Pets/AnimalPetSmokeTest.cs
+++ src/FACM/Pets/AnimalPetSmokeTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace FACM.Pets
@@ -18,37 +19,46 @@ namespace FACM.Pets
                     throw new InvalidOperationException("Built-in animal pet catalog is unexpectedly small.");
 
                 var signatures = new HashSet<int>();
-                foreach (var pet in AnimalPetCatalog.All)
+                using (var cancellation = new CancellationTokenSource(TimeSpan.FromSeconds(30)))
                 {
-                    using (var bitmap = AnimalPetWindow.RenderForSmokeTest(pet, 1.17f, true))
+                    foreach (var pet in AnimalPetCatalog.All)
                     {
-                        if (bitmap.Width < 96 || bitmap.Height < 96)
-                            throw new InvalidOperationException("Animal pet render is unexpectedly small: " + pet.Id);
-                        if (bitmap.GetPixel(0, 0).A > 3 ||
-                            bitmap.GetPixel(bitmap.Width - 1, 0).A > 3 ||
-                            bitmap.GetPixel(0, bitmap.Height - 1).A > 3 ||
-                            bitmap.GetPixel(bitmap.Width - 1, bitmap.Height - 1).A > 3)
-                            throw new InvalidOperationException("Animal pet corners are not transparent: " + pet.Id);
-
-                        var visible = 0;
-                        var alphaEdges = 0;
-                        var signature = 17;
-                        for (var y = 0; y < bitmap.Height; y += 2)
+                        using (var artwork = AnimalPetArtService.LoadAsync(pet, cancellation.Token).GetAwaiter().GetResult())
                         {
-                            for (var x = 0; x < bitmap.Width; x += 2)
+                            if (artwork == null || artwork.Width < 64 || artwork.Height < 64)
+                                throw new InvalidOperationException("Professional animal artwork could not be loaded: " + pet.Id);
+
+                            using (var bitmap = AnimalPetWindow.RenderForSmokeTest(pet, artwork, 1.17f, true))
                             {
-                                var pixel = bitmap.GetPixel(x, y);
-                                if (pixel.A > 80) visible++;
-                                if (pixel.A > 5 && pixel.A < 220) alphaEdges++;
-                                if (pixel.A > 100)
-                                    signature = unchecked(signature * 31 + pixel.ToArgb());
+                                if (bitmap.Width < 120 || bitmap.Height < 120)
+                                    throw new InvalidOperationException("Animal pet render is unexpectedly small: " + pet.Id);
+                                if (bitmap.GetPixel(0, 0).A > 3 ||
+                                    bitmap.GetPixel(bitmap.Width - 1, 0).A > 3 ||
+                                    bitmap.GetPixel(0, bitmap.Height - 1).A > 3 ||
+                                    bitmap.GetPixel(bitmap.Width - 1, bitmap.Height - 1).A > 3)
+                                    throw new InvalidOperationException("Animal pet corners are not transparent: " + pet.Id);
+
+                                var visible = 0;
+                                var alphaEdges = 0;
+                                var signature = 17;
+                                for (var y = 0; y < bitmap.Height; y += 2)
+                                {
+                                    for (var x = 0; x < bitmap.Width; x += 2)
+                                    {
+                                        var pixel = bitmap.GetPixel(x, y);
+                                        if (pixel.A > 80) visible++;
+                                        if (pixel.A > 5 && pixel.A < 220) alphaEdges++;
+                                        if (pixel.A > 100)
+                                            signature = unchecked(signature * 31 + pixel.ToArgb());
+                                    }
+                                }
+                                if (visible < 450)
+                                    throw new InvalidOperationException("Animal pet render contains too little visible content: " + pet.Id);
+                                if (alphaEdges < 20)
+                                    throw new InvalidOperationException("Animal pet render has no anti-aliased transparent edge: " + pet.Id);
+                                signatures.Add(signature);
                             }
                         }
-                        if (visible < 300)
-                            throw new InvalidOperationException("Animal pet render contains too little visible content: " + pet.Id);
-                        if (alphaEdges < 20)
-                            throw new InvalidOperationException("Animal pet render has no anti-aliased transparent edge: " + pet.Id);
-                        signatures.Add(signature);
                     }
                 }
 
