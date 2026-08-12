da0d81df5742727cf358b9f22f1437beaacf052a
xianyumht-cmd <xianyumht@gmail.com>
2026-08-13 05:13:03 +0800

    refactor(core): add Mayhem module facade

---
+++ src/FACM/Application/Modules/MayhemModule.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using FACM.Mayhem;
+
+namespace FACM.Application.Modules
+{
+    internal sealed class MayhemModule : IFacmModule
+    {
+        private static readonly IReadOnlyList<string> NoDependencies = Array.Empty<string>();
+
+        public const string ModuleId = "mayhem";
+
+        public string Id
+        {
+            get { return ModuleId; }
+        }
+
+        public IReadOnlyList<string> Dependencies
+        {
+            get { return NoDependencies; }
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public Form CreateLookupForm()
+        {
+            return new MayhemLookupForm();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
