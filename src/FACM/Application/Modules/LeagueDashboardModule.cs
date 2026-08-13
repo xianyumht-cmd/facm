6f8369c026b2ee4f1356dfd6335aa993896c7555
xianyumht-cmd <xianyumht@gmail.com>
2026-08-14 02:39:29 +0800

    feat(league): add dashboard module ownership

---
+++ src/FACM/Application/Modules/LeagueDashboardModule.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using FACM.AppHost;
+using FACM.Performance;
+using FACM.Services;
+
+namespace FACM.AppHost.Modules
+{
+    internal sealed class LeagueDashboardModule : IFacmModule
+    {
+        private static readonly IReadOnlyList<string> ModuleDependencies = new[]
+        {
+            LeagueClientModule.ModuleId,
+            PerformanceModule.ModuleId
+        };
+
+        private readonly LeagueClientModule _leagueClient;
+        private readonly PerformanceModule _performance;
+
+        public LeagueDashboardModule(LeagueClientModule leagueClient, PerformanceModule performance)
+        {
+            _leagueClient = leagueClient ?? throw new ArgumentNullException(nameof(leagueClient));
+            _performance = performance ?? throw new ArgumentNullException(nameof(performance));
+        }
+
+        public const string ModuleId = "league-dashboard";
+        public string Id { get { return ModuleId; } }
+        public IReadOnlyList<string> Dependencies { get { return ModuleDependencies; } }
+        public void Initialize() { }
+
+        public Form CreateDashboardForm(UiTextCatalog ui)
+        {
+            return new LeagueDashboardForm(_leagueClient, _performance.Budgets, ui);
+        }
+
+        public void Dispose() { }
+    }
+}
