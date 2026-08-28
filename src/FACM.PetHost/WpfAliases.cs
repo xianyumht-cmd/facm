c97ba93c7f21f0d3ebc4837db896c3486631035f
xianyumht-cmd <xianyumht@gmail.com>
2026-08-28 14:22:03 +0800

    fix(personalization): disambiguate PetHost WPF types

---
+++ src/FACM.PetHost/WpfAliases.cs
@@ -0,0 +1,5 @@
+global using Grid = System.Windows.Controls.Grid;
+global using Color = System.Windows.Media.Color;
+global using Rectangle = System.Windows.Shapes.Rectangle;
+global using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+global using Point = System.Windows.Point;
